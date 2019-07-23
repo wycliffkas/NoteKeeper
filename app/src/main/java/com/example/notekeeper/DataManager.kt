6eff0dc164383a29242de216551cc21883bd4309
w4wycliff <w4wycliff@gmail.com>
2019-07-23 13:48:22 +0300

    adding model classes and data manager

---
+++ app/src/main/java/com/example/notekeeper/DataManager.kt
@@ -0,0 +1,25 @@
+package com.example.notekeeper
+
+class DataManager {
+    val courses = HashMap<String, CourseInfo>()
+    val notes = ArrayList<NoteInfo>()
+
+    init {
+        initialiseCourses()
+    }
+
+    private fun initialiseCourses(){
+        var course = CourseInfo("android_intents", "Android Programming with Intents")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo("android_async", "Android Async Programming Services")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo("java_lang", "Java Fundamentals: The java language")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo("java core", "Java Fundamentals: The core platform")
+        courses.set(course.courseId, course)
+
+    }
+}
